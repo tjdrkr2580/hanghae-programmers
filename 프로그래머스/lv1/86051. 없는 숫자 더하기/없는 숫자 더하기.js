0c4db3a7df8dd093b82d74b3216640fbf8b15680
김태현 <97386129+tjdrkr2580@users.noreply.github.com>
2023-01-22 10:08:18 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,12 +1,6 @@
 function solution(numbers, sum = 0) {
-     numbers.includes(1) === true ? null : sum += 1
-     numbers.includes(2) === true ? null : sum += 2
-     numbers.includes(3) === true ? null : sum += 3 
-     numbers.includes(4) === true ? null : sum += 4 
-     numbers.includes(5) === true ? null : sum += 5 
-     numbers.includes(6) === true ? null : sum += 6 
-     numbers.includes(7) === true ? null : sum += 7 
-     numbers.includes(8) === true ? null : sum += 8 
-     numbers.includes(9) === true ? null : sum += 9
+    for(let i = 1; i <= 9 ; i++) {
+        if(!numbers.includes(i) === true) sum += i;
+    }
     return sum;
 }
