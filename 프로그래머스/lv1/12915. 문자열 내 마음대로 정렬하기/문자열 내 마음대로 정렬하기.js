fefb2b85926692fd5181a14e18e997f29bc1b5e3
김태현 <97386129+tjdrkr2580@users.noreply.github.com>
2023-01-28 14:02:59 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.58 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,9 +1,9 @@
 function solution(strings, n) {
     strings.sort((a,b) => {
-       if(a.split("")[n] < b.split("")[n]) return -1
-        else if(a.split("")[n] == b.split("")[n]){
-            if(a > b) return 1
-            else return -1
+        if(a.split('')[n] < b.split('')[n]) return -1;
+        else if(a.split('')[n] === b.split('')[n]){
+            if(a > b) return 1;
+            else return -1;
         }
         else return 1;
     })
