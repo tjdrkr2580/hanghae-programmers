61a7d31e3fe9d9711a5b35646cf5f16d3621ef9d
김태현 <97386129+tjdrkr2580@users.noreply.github.com>
2023-01-29 15:35:17 +0900

    [level 1] Title: 시저 암호, Time: 1.35 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -1,20 +1,21 @@
     function solution(s, n) {
-      let arr = s.split("").map((element) => element.charCodeAt());
-      arr = arr.map((element) => {
-        console.log(element);
-           if (element >= 65 && element <= 90) {
-            element += n;
-        if (element > 90) {
-          element -= 26;
-        }
-      }
-      else if (element >= 97 && element <= 122) {
-        element += n;
-        if (element > 122) {
-          element -= 26;
-        }
-      }
+      s = s.split("").map((element) => element.charCodeAt());
+      s = s.map((element) => {
+        if (element === 32) {
           return String.fromCharCode(element);
+        } else if (element >= 65 && element <= 90) {
+          element += n;
+          if (element > 90) {
+            return String.fromCharCode(element - 26);
+          }
+          return String.fromCharCode(element);
+        } else if (element >= 97 && element <= 122) {
+          element += n;
+          if (element > 122) {
+            return String.fromCharCode(element - 26);
+          }
+          return String.fromCharCode(element);
+        }
       });
-        return arr.join('')
+      return s.join("");
     }
