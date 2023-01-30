c6884c6461c65305eb4a19d45a787b02550b2438
김태현 <97386129+tjdrkr2580@users.noreply.github.com>
2023-01-30 15:13:53 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,5 +1,4 @@
-function solution(x) {
-    let slice_num = 0;
+function solution(x, slice_num = 0) {
     String(x).split('').map((x) => slice_num += parseInt(x));
     return x % slice_num == 0;
 }
