22cb424158d68d4b758771fa00ad1f1786dcf77a
김태현 <97386129+tjdrkr2580@users.noreply.github.com>
2023-01-30 14:58:49 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/README.md
+++ 프로그래머스/lv1/12982. 예산/README.md
@@ -1,6 +1,6 @@
 # [level 1] 예산 - 12982 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12982) 
 
 ### 성능 요약
 
--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,14 +1,10 @@
 function solution(d, budget, count = 0) {
-    d.sort((a,b) => a - b);
+    d.sort((a,b) => a-b)
     let sum = 0;
     for(let i = 0 ; i < d.length ; i++) {
         sum += d[i];
-        if(budget >= sum) {
-            count ++;
-        }
-        else {
-            break;
-        }
+        if(sum <= budget) count++;
+        else break;
     }
-    return count
+    return count;
 }
